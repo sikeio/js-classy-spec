6a79b26a5551264f3da3ae6542b679fcc8674907
Howard Yeh <howard@metacircus.com>
2016-03-31 12:05:44 +0800

    reformat

--- class_spec.js
+++ class_spec.js
@@ -1,39 +1,40 @@
-var expect = require("chai").expect;
-var Class = require("../");
+"use strict";
+const expect = require("chai").expect;
+const Class = require("../");
 
-describe("Implement Class Constructor",function() {
-  var constructor = function(a,b) {
+describe("Implement Class Constructor", function() {
+  const constructor = function(a, b) {
     this.a = a;
     this.b = b;
   };
 
-  var Foo = Class({initialize: constructor});
+  const Foo = Class({ initialize: constructor });
 
-  it("should return a class constructor function",function() {
+  it("should return a class constructor function", function() {
     expect(Foo).to.be.a("function");
   });
 
-  it("should be able define a class",function() {
-    obj = new Foo(1,2);
+  it("should be able define a class", function() {
+    const obj = new Foo(1, 2);
     expect(obj.constructor).to.eq(Foo);
     expect(obj.a).to.eql(1);
     expect(obj.b).to.eql(2);
 
-    obj2 = new Foo(3,4);
+    const obj2 = new Foo(3, 4);
     expect(obj2.a).to.eql(3);
     expect(obj2.b).to.eql(4);
   });
 
-  it("should be able define a class without constructor",function() {
-    klass = Class({});
-    obj = new klass()
+  it("should be able define a class without constructor", function() {
+    const klass = Class({});
+    const obj = new klass()
     expect(obj.constructor).to.eq(klass);
   });
 });
 
-describe("Implement Instance Methods",function() {
-  var Foo = Class({
-    initialize: function(a,b) {
+describe("Implement Instance Methods", function() {
+  const Foo = Class({
+    initialize: function(a, b) {
       this.a = a;
       this.b = b;
     },
@@ -47,9 +48,9 @@ describe("Implement Instance Methods",function() {
     }
   });
 
-  var foo = new Foo(1,2);
+  const foo = new Foo(1, 2);
 
-  it("should be able to define methods",function() {
+  it("should be able to define methods", function() {
     expect(foo.getA).to.be.a("function");
     expect(foo.getB).to.be.a("function");
 
@@ -57,7 +58,7 @@ describe("Implement Instance Methods",function() {
     expect(foo.getB()).to.eq(2);
   });
 
-  it("should not define `initialize` as a method",function() {
+  it("should not define `initialize` as a method", function() {
     expect(foo.initialize).to.be.undefined;
   });
 });
