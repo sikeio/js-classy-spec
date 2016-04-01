d2c8c0190629503aac09fe4095b32d33afaa2223
Howard Yeh <howard@metacircus.com>
2016-04-01 16:17:18 +0800

    reformat code

--- inheritance_spec.js
+++ inheritance_spec.js
@@ -19,37 +19,60 @@ describe("Implement Methods Inheritance", function() {
   const b = new B();
   const a = new A();
 
-  describe("b",function() {
-    it("constructor should be B",function() {
+  describe("b", function() {
+    it("constructor should be B", function() {
       assert(b.constructor === B);
     });
 
-    it("should be an instance of A",function() {
+    it("should be an instance of A", function() {
       assert(b instanceof A);
     });
 
-    it("should be an instance of B",function() {
+    it("should be an instance of B", function() {
       assert(b instanceof B);
     });
 
-    it("should be able to call method `a` through inheritance",function() {
+    it("should be able to call method `a` through inheritance", function() {
       assert.equal(b.a(), 1);
     });
 
-    it("should not have method `a` defined directly on the object",function() {
+    it("should not have method `a` defined directly on the object", function() {
       assert(b.hasOwnProperty("a") === false);
     });
 
-    it("should not have method `a` defined directly on the prototype of B",function() {
+    it("should not have method `a` defined directly on the prototype of B", function() {
       assert(b.__proto__.hasOwnProperty("a") === false);
     });
   });
 });
 
-describe("Implement Super call",function() {
+
+describe("Implement Class __super__", function() {
   var A = Class({
-    foo: function(a,b) {
-      return [this.n,a,b];
+    a: function() {
+      return 1;
+    }
+  });
+
+  var B = Class({
+    b: function() {
+      return 2;
+    }
+  }, A);
+
+  it("should set the __super__ class property to the parent class", function() {
+    expect(B.__super__).to.eq(A);
+  });
+
+  it("should set Object as the default __super__ class", function() {
+    expect(A.__super__).to.eq(Object);
+  });
+});
+
+describe("Implement Super call", function() {
+  var A = Class({
+    foo: function(a, b) {
+      return [this.n, a, b];
     },
 
     bar: function() {
@@ -62,8 +85,8 @@ describe("Implement Super call",function() {
   });
 
   var B = Class({
-    foo: function(a,b) {
-      return this.super("foo",a*10,b*100);
+    foo: function(a, b) {
+      return this.super("foo", a * 10, b * 100);
     },
 
     bar: function() {
@@ -73,13 +96,13 @@ describe("Implement Super call",function() {
     self: function() {
       return this.super("self");
     }
-  },A);
+  }, A);
 
   var C = Class({
-    foo: function(a,b) {
-      return this.super("foo",a*10,b*100);
+    foo: function(a, b) {
+      return this.super("foo", a * 10, b * 100);
     }
-  },B);
+  }, B);
 
   var b = new B();
   b.n = 1;
@@ -88,7 +111,7 @@ describe("Implement Super call",function() {
     expect(b.super).to.be.a("function");
   });
 
-  it("should be able to call super method without arguments",function() {
+  it("should be able to call super method without arguments", function() {
     expect(b.bar()).to.equal(21);
   });
 
@@ -97,59 +120,36 @@ describe("Implement Super call",function() {
   });
 
   it("should be able to call super method with multiple arguments", function() {
-    expect(b.foo(2,3)).to.deep.equal([1,20,300]);
+    expect(b.foo(2, 3)).to.deep.equal([1, 20, 300]);
   });
 
 });
 
-describe("Implement Class __super__",function() {
-  var A = Class({
-    a: function() {
-      return 1;
-    }
-  });
-
-  var B = Class({
-    b: function() {
-      return 2;
-    }
-  },A);
-
-  it("should set the __super__ class property to the parent class",function() {
-    expect(B.__super__).to.eq(A);
-  });
-
-  it("should set Object as the default __super__ class",function() {
-    expect(A.__super__).to.eq(Object);
-  });
-});
-
-
-describe("Implement Super's Super",function() {
+describe("Implement Super's Super", function() {
   var A, B, C, c;
   beforeEach(function() {
     A = Class({
-      foo: function(a,b) {
-        return [a,b];
+      foo: function(a, b) {
+        return [a, b];
       }
     });
 
     B = Class({
-      foo: function(a,b) {
-        return this.super("foo",a*10,b*100);
+      foo: function(a, b) {
+        return this.super("foo", a * 10, b * 100);
       }
-    },A);
+    }, A);
 
     C = Class({
-      foo: function(a,b) {
-        return this.super("foo",a*10,b*100);
+      foo: function(a, b) {
+        return this.super("foo", a * 10, b * 100);
       }
-    },B);
+    }, B);
 
     c = new C();
   });
 
-  it("should be able to call super's super",function() {
-    expect(c.foo(1,2)).to.deep.equal([100,20000]);
+  it("should be able to call super's super", function() {
+    expect(c.foo(1, 2)).to.deep.equal([100, 20000]);
   });
 });
